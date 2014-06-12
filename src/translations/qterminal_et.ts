fff3774510ac437b8635f8a0b6c2d7f20b9cef86
Petr Vanek <petr@scribus.info>
2014-06-12 12:15:36 +0200

    estonian translation

---
+++ src/translations/qterminal_et.ts
@@ -0,0 +1,355 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="et_EE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="83"/>
+        <source>Add Tab</source>
+        <translation>Lisa kaart</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="89"/>
+        <source>Close Tab</source>
+        <translation>Sulge kaart</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="97"/>
+        <source>Split Terminal Horizontally</source>
+        <translation>Poolita terminal horisontaalselt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="103"/>
+        <source>Split Terminal Vertically</source>
+        <translation>Poolita terminal vertikaalselt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="109"/>
+        <source>Collapse Subterminal</source>
+        <translation>Ahenda alamterminal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="115"/>
+        <source>Next Subterminal</source>
+        <translation>Järgmine alamterminal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="121"/>
+        <source>Previous Subterminal</source>
+        <translation>Eelmine alamterminal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="129"/>
+        <source>Next Tab</source>
+        <translation>Järgmine kaart</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="135"/>
+        <source>Previous Tab</source>
+        <translation>Eelmine kaart</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="141"/>
+        <source>Move Tab Left</source>
+        <translation>Liiguta kaart vasakule</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="147"/>
+        <source>Move Tab Right</source>
+        <translation>Liiguta kaart paremale</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="156"/>
+        <source>Copy Selection</source>
+        <translation>Kopeeri valik</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="160"/>
+        <source>Paste Selection</source>
+        <translation>Aseta valik</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="170"/>
+        <source>Save Session</source>
+        <translation>Salvesta seanss</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <source>Load Session</source>
+        <translation>Lae seanss</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="197"/>
+        <source>Toggle Borderless</source>
+        <translation>Lülita ääristeta režiimile</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="203"/>
+        <source>Toggle TabBar</source>
+        <translation>Lülita kaardiriba</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="213"/>
+        <source>Bottom</source>
+        <translation>All</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="214"/>
+        <source>Top</source>
+        <translation>Ülal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="215"/>
+        <location filename="../mainwindow.cpp" line="246"/>
+        <source>Right</source>
+        <translation>Paremal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="216"/>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>Left</source>
+        <translation>Vasakul</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="231"/>
+        <source>Tabs Layout</source>
+        <translation>Kaartide paigutus</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="245"/>
+        <source>None</source>
+        <translation>Puudub</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="263"/>
+        <source>Scrollbar Layout</source>
+        <translation>Kerimisriba paigutus</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="306"/>
+        <source>Exit QTerminal</source>
+        <translation>Lõpeta Qterminali töö</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="308"/>
+        <source>Do not ask again</source>
+        <translation>Ära küsi uuesti</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="315"/>
+        <source>Are you sure you want to exit?</source>
+        <translation>Soovid sa tõesti väljuda?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="343"/>
+        <source>A lightweight multiplatform terminal emulator</source>
+        <translation>Ressursisäästlik multiplatvormne terminaliemulaator</translation>
+    </message>
+</context>
+<context>
+    <name>PropertiesDialog</name>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="14"/>
+        <source>QTerminal Properties</source>
+        <translation>QTerminali omadused</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="39"/>
+        <source>Terminal Font</source>
+        <translation>Terminali font</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="44"/>
+        <source>Look and Feel</source>
+        <translation>Tööilme</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="49"/>
+        <location filename="../forms/propertiesdialog.ui" line="239"/>
+        <source>History</source>
+        <translation>Ajalugu</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="54"/>
+        <source>Emulation</source>
+        <translation>Emuleering</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="59"/>
+        <source>Shortcuts</source>
+        <translation>Otseteed</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="77"/>
+        <source>Font</source>
+        <translation>Font</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="109"/>
+        <source>&amp;Set Font...</source>
+        <translation>&amp;Määra font...</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="136"/>
+        <source>Color Scheme:</source>
+        <translation>Värviskeem:</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="146"/>
+        <source>GUI Style:</source>
+        <translation>GUI stiil:</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="156"/>
+        <source>Scrollbar Position:</source>
+        <translation>Kerimisriba asukoht:</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="166"/>
+        <source>Tabs Position:</source>
+        <translation>Kaartide asukoht:</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="176"/>
+        <source>&amp;Application Transparency:</source>
+        <translation>&amp;Rakenduse läbipaistvus:</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="186"/>
+        <location filename="../forms/propertiesdialog.ui" line="222"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="202"/>
+        <source>Terminal Transparency:</source>
+        <translation>Terminali läbipaistvus:</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="245"/>
+        <source>Limited to: </source>
+        <translation>Piiratud kuni: </translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="265"/>
+        <source>Unlimited</source>
+        <translation>Piiramatu</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="292"/>
+        <source>Emulation Type</source>
+        <translation>Emuleeringu tüüp</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="301"/>
+        <source>Select emulation behaviour. It does not mean that for example &quot;linux&quot; has to be used only on Linux OS. Feel free to use it on Mac too etc.&lt;p&gt;Default is the failback type with minimal feature set.</source>
+        <translation>Vali emuleeringu käitumine. See ei tähenda, et näiteks &quot;linux&quot; tohib olla kasutusel vaid Linux OS puhul. Kasuta seda vabalt ka Mac&apos;l jne.&lt;p&gt;Vaikimisi varuvariant on minimaalsete võimalustega.</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="353"/>
+        <source>Shortcut</source>
+        <translation>Otsetee</translation>
+    </message>
+    <message>
+        <location filename="../forms/propertiesdialog.ui" line="358"/>
+        <source>Key</source>
+        <translation>Klahv</translation>
+    </message>
+    <message>
+        <location filename="../propertiesdialog.cpp" line="50"/>
+        <location filename="../propertiesdialog.cpp" line="85"/>
+        <source>System Default</source>
+        <translation>Süsteemne vaikeväärtus</translation>
+    </message>
+    <message>
+        <location filename="../propertiesdialog.cpp" line="121"/>
+        <source>Select Terminal Font</source>
+        <translation>Vali terminali font</translation>
+    </message>
+</context>
+<context>
+    <name>TabWidget</name>
+    <message>
+        <location filename="../tabwidget.cpp" line="74"/>
+        <location filename="../tabwidget.cpp" line="135"/>
+        <source>Shell No. %1</source>
+        <translation>Shell No. %1</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.cpp" line="123"/>
+        <source>Tab name</source>
+        <translation>Kaardi nimi</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.cpp" line="124"/>
+        <source>New tab name:</source>
+        <translation>Kaardi uus nimi:</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.cpp" line="157"/>
+        <source>Close session</source>
+        <translation>Sulge seanss</translation>
+    </message>
+    <message>
+        <location filename="../tabwidget.cpp" line="159"/>
+        <source>Rename session</source>
+        <translation>Nimeta seanss ümber</translation>
+    </message>
+</context>
+<context>
+    <name>TermWidgetHolder</name>
+    <message>
+        <location filename="../termwidgetholder.cpp" line="40"/>
+        <source>Load Session</source>
+        <translation>Lae seanss</translation>
+    </message>
+    <message>
+        <location filename="../termwidgetholder.cpp" line="41"/>
+        <source>List of saved sessions:</source>
+        <translation>Salvestatud seansside nimekiri:</translation>
+    </message>
+</context>
+<context>
+    <name>mainWindow</name>
+    <message>
+        <location filename="../forms/qterminal.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>PeaAken</translation>
+    </message>
+    <message>
+        <location filename="../forms/qterminal.ui" line="59"/>
+        <source>Actions</source>
+        <translation>Tegevused</translation>
+    </message>
+    <message>
+        <location filename="../forms/qterminal.ui" line="67"/>
+        <source>Help</source>
+        <translation>Abi</translation>
+    </message>
+    <message>
+        <location filename="../forms/qterminal.ui" line="74"/>
+        <source>&amp;Window</source>
+        <translation>&amp;Aken</translation>
+    </message>
+    <message>
+        <location filename="../forms/qterminal.ui" line="83"/>
+        <source>About...</source>
+        <translation>QTerminali info...</translation>
+    </message>
+    <message>
+        <location filename="../forms/qterminal.ui" line="88"/>
+        <source>About Qt...</source>
+        <translation>Qt info...</translation>
+    </message>
+    <message>
+        <location filename="../forms/qterminal.ui" line="93"/>
+        <source>&amp;Preferences...</source>
+        <translation>&amp;Seadistused...</translation>
+    </message>
+    <message>
+        <location filename="../forms/qterminal.ui" line="102"/>
+        <source>&amp;Quit...</source>
+        <translation>&amp;Välju...</translation>
+    </message>
+</context>
+</TS>
