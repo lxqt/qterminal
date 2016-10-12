882233021e5a11db831e2eee6758854bcc9b37e2
Palo Kisa <palo.kisa@gmail.com>
2016-10-12 12:19:15 +0200

    Fix language vs. name discrepancies
    ...revealed by auto testing

--- qterminal_et.ts
+++ qterminal_et.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="et_EE">
+<TS version="2.1" language="et">
 <context>
     <name>BookmarksWidget</name>
     <message>
