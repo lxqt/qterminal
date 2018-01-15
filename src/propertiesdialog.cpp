lxqt/qterminal

commit 23fb6f84fdb39ad3ebcfa10c242e8ee51ade9b7d

Drop Qt foreach
Replaced by the ranged-based for loop.
Qt 5.7.1 is required. We use qAsConst().

src/propertiesdialog.cpp
@@ -266,7 +266,8 @@ void PropertiesDialog::saveShortcuts()
         QString sequenceString = sequence.toString();
 
         QList<QKeySequence> shortcuts;
-        foreach (sequenceString, item->text().split('|'))
+        const auto sequences = item->text().split('|');
+        for (const QKeySequence& hsequenceString : sequences)
             shortcuts.append(QKeySequence(sequenceString));
         keyAction->setShortcuts(shortcuts);
     }
@@ -287,7 +288,8 @@ void PropertiesDialog::setupShortcuts()
         QAction *keyAction = actions[keyValue];
         QStringList sequenceStrings;
 
-        foreach (const QKeySequence &shortcut, keyAction->shortcuts())
+        const auto shortcuts = keyAction->shortcuts();
+        for (const QKeySequence &shortcut : shortcuts)
             sequenceStrings.append(shortcut.toString());
 
         QTableWidgetItem *itemName = new QTableWidgetItem( tr(keyValue.toStdString().c_str()) );

CMakeLists.txt
@@ -6,7 +6,7 @@ include(GNUInstallDirs)
 
 # qterminal version
 set(STR_VERSION "0.8.0")
-set(LXQTBT_MINIMUM_VERSION "0.4.0")
+set(LXQTBT_MINIMUM_VERSION "0.4.1")
 set(QTERMWIDGET_MINIMUM_VERSION "0.8.0")
 
 option(UPDATE_TRANSLATIONS "Update source translation translations/*.ts files" OFF)
@@ -43,7 +43,10 @@ elseif(UNIX)
    message(STATUS "X11_X11_LIB: ${X11_X11_LIB}")
 endif()
 
-add_definitions(-DSTR_VERSION=\"${STR_VERSION}\")
+add_definitions(
+    -DSTR_VERSION=\"${STR_VERSION}\"
+    -DQT_NO_FOREACH
+)
 
 set(EXE_NAME qterminal)
 

src/bookmarkswidget.cpp
@@ -123,7 +123,7 @@ public:
         QDir d;
 
         // standard $HOME subdirs
-        foreach (QStandardPaths::StandardLocation i, locations)
+        for (const QStandardPaths::StandardLocation i : qAsConst(locations))
         {
             path = QStandardPaths::writableLocation(i);
             if (!d.exists(path))
@@ -140,7 +140,8 @@ public:
 
         // system env - include dirs in the tree
         QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
-        foreach (const QString &i, env.keys())
+        const auto keys = env.keys();
+        for (const QString &i : keys)
         {
             path = env.value(i);
             if (!d.exists(path) || !QFileInfo(path).isDir())

src/main.cpp
@@ -267,7 +267,7 @@ void QTerminalApp::registerOnDbus()
 QList<QDBusObjectPath> QTerminalApp::getWindows()
 {
     QList<QDBusObjectPath> windows;
-    foreach (MainWindow *wnd, m_windowList)
+    for (MainWindow *wnd : qAsConst(m_windowList))
     {
         windows.push_back(wnd->getDbusPath());
     }

src/mainwindow.cpp
@@ -178,7 +178,8 @@ void MainWindow::setup_Action(const char *name, QAction *action, const char *def
     QList<QKeySequence> shortcuts;
 
     actions[name] = action;
-    foreach (const QString &sequenceString, settings.value(name, defaultShortcut).toString().split('|'))
+    const auto sequences = settings.value(name, defaultShortcut).toString().split('|');
+    for (const QString &sequenceString : sequences)
         shortcuts.append(QKeySequence::fromString(sequenceString));
     actions[name]->setShortcuts(shortcuts);
 

src/properties.cpp
@@ -165,7 +165,8 @@ void Properties::saveSettings()
     {
         it.next();
         QStringList sequenceStrings;
-        foreach (const QKeySequence &shortcut, it.value()->shortcuts())
+        const auto shortcuts = it.value()->shortcuts();
+        for (const QKeySequence &shortcut : shortcuts)
             sequenceStrings.append(shortcut.toString());
         m_settings->setValue(it.key(), sequenceStrings.join('|'));
     }

src/termwidget.cpp
@@ -230,7 +230,8 @@ void TermWidgetImpl::paste(QClipboard::Mode mode)
                 confirmation.setDetailedText(text);
                 confirmation.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
                 // Click "Show details..." to show those by default
-                foreach( QAbstractButton * btn, confirmation.buttons() )
+                const auto buttons = confirmation.buttons();
+                for( QAbstractButton * btn : buttons )
                 {
                     if (confirmation.buttonRole(btn) == QMessageBox::ActionRole && btn->text() == QMessageBox::tr("Show Details..."))
                     {
@@ -287,7 +288,8 @@ TermWidget::TermWidget(TerminalConfig &cfg, QWidget * parent)
     setLayout(m_layout);
 
     m_layout->addWidget(m_term);
-    foreach (QObject *o, m_term->children())
+    const auto objs = m_term->children();
+    for (QObject *o : objs)
     {
         // Find TerminalDisplay
         if (!o->isWidgetType() || qobject_cast<QWidget*>(o)->isHidden())

src/termwidgetholder.cpp
@@ -118,10 +118,12 @@ void TermWidgetHolder::saveSession(const QString & name)
 {
     Session dump;
     QString num("%1");
-    foreach(QSplitter *w, findChildren<QSplitter*>())
+    const auto ws = findChildren<QSplitter*>();
+    for(QSplitter *w : ws)
     {
         dump += '|' + num.arg(w->orientation());
-        foreach (int i, w->sizes())
+        const auto sizes = w->sizes();
+        for (const int i : sizes)
             dump += ',' + num.arg(i);
     }
     Properties::Instance()->sessions[name] = dump;
@@ -198,7 +200,7 @@ void TermWidgetHolder::directionalNavigation(NavigationDirection dir) {
     // Find an active widget
     QList<TermWidget*> l = findChildren<TermWidget*>();
     int ix = -1;
-    foreach (TermWidget * w, l)
+    for (TermWidget * w : qAsConst(l))
     {
         ++ix;
         if (w->impl()->hasFocus())
@@ -230,7 +232,7 @@ void TermWidgetHolder::directionalNavigation(NavigationDirection dir) {
     int lowestX = INT_MAX;
     int lowestMidpointDistance = INT_MAX;
     TermWidget *fittest = NULL;
-    foreach (TermWidget * w, l) 
+    for (TermWidget * w : qAsConst(l))
     {
         NavigationData contenderDims = getNormalizedDimensions(w, dir);
         int midpointDistance = std::min(
@@ -260,7 +262,8 @@ void TermWidgetHolder::clearActiveTerminal()
 
 void TermWidgetHolder::propertiesChanged()
 {
-    foreach(TermWidget *w, findChildren<TermWidget*>())
+    const auto ws = findChildren<TermWidget*>();
+    for(TermWidget *w : ws)
         w->propertiesChanged();
 }
 
@@ -423,7 +426,8 @@ QDBusObjectPath TermWidgetHolder::getActiveTerminal()
 QList<QDBusObjectPath> TermWidgetHolder::getTerminals()
 {
     QList<QDBusObjectPath> terminals;
-    foreach (TermWidget* w, findChildren<TermWidget*>())
+    const auto ws = findChildren<TermWidget*>();
+    for (TermWidget* w : ws)
     {
         terminals.push_back(w->getDbusPath());
     }

commit 32d27a9856f6009c180f90795ed487c745225007

Fix a typo
Introduced by commit 23fb6f84fdb3.
Not caught due to so many warnings.... one more, one less... nobody notices
it.

src/propertiesdialog.cpp
@@ -270,7 +270,7 @@ void PropertiesDialog::saveShortcuts()
 
         QList<QKeySequence> shortcuts;
         const auto sequences = item->text().split('|');
-        for (const QKeySequence& hsequenceString : sequences)
+        for (const QKeySequence& sequenceString : sequences)
             shortcuts.append(QKeySequence(sequenceString));
         keyAction->setShortcuts(shortcuts);
     }