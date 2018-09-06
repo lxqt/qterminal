8256799159e774a4fd5f962375d83ff5da93a8b4
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2018-09-06 20:23:16 +0000

    Translated using Weblate (Catalan)
    Currently translated at 100,0% (158 of 158 strings)
    
    Translation: LXQt/QTerminal
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qterminal/ca/

--- src/translations/qterminal_ca.ts
+++ src/translations/qterminal_ca.ts
@@ -88,12 +88,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="225"/>
         <source>&amp;Previous Tab in History</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Pestanya anterior a l&apos;historial</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="228"/>
         <source>&amp;Next Tab in History</source>
-        <translation type="unfinished"></translation>
+        <translation>Pestanya següe&amp;nt a l&apos;historial</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="250"/>
@@ -475,7 +475,7 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="385"/>
         <source>Fixed tab width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Amplada fixa de la pestanya:</translation>
     </message>
     <message>
         <source>Limit tab width:</source>
