223d89f31fa0abaa8f74c9c773d94626a24381f1
scootergrisen <scootergrisen@gmail.com>
2018-09-08 06:30:02 +0000

    Translated using Weblate (Danish)
    Currently translated at 100,0% (158 of 158 strings)
    
    Translation: LXQt/QTerminal
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qterminal/da/

--- src/translations/qterminal_da.ts
+++ src/translations/qterminal_da.ts
@@ -88,12 +88,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="225"/>
         <source>&amp;Previous Tab in History</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Forrige faneblad i historik</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="228"/>
         <source>&amp;Next Tab in History</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Næste faneblad i historik</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="250"/>
@@ -475,7 +475,7 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="385"/>
         <source>Fixed tab width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Fast brede for faneblad:</translation>
     </message>
     <message>
         <source>Limit tab width:</source>
