3c592b8674dee119558793f311c881c1f8859677
Yaron Shahrabani <sh.yaron@gmail.com>
2018-09-12 11:41:11 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (158 of 158 strings)
    
    Translation: LXQt/QTerminal
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qterminal/he/

--- src/translations/qterminal_he.ts
+++ src/translations/qterminal_he.ts
@@ -88,12 +88,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="225"/>
         <source>&amp;Previous Tab in History</source>
-        <translation type="unfinished"></translation>
+        <translation>הלשונית ה&amp;קודמת בהיסטוריה</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="228"/>
         <source>&amp;Next Tab in History</source>
-        <translation type="unfinished"></translation>
+        <translation>הלשונית ה&amp;באה בהיסטוריה</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="250"/>
@@ -475,7 +475,7 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="385"/>
         <source>Fixed tab width:</source>
-        <translation type="unfinished"></translation>
+        <translation>רוחב לשונית קבוע:</translation>
     </message>
     <message>
         <source>Limit tab width:</source>
