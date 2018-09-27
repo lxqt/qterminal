3d9c6731205748d8049171f8bf37d0e8c9a15381
Marcin Mikołajczak <marcin123407@vivaldi.net>
2018-09-27 10:47:55 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (158 of 158 strings)
    
    Translation: LXQt/QTerminal
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qterminal/pl/

--- src/translations/qterminal_pl.ts
+++ src/translations/qterminal_pl.ts
@@ -88,12 +88,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="225"/>
         <source>&amp;Previous Tab in History</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Poprzednia karta w historii</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="228"/>
         <source>&amp;Next Tab in History</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Następna karta w historii</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="250"/>
@@ -475,7 +475,7 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="385"/>
         <source>Fixed tab width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Stała szerokość kart:</translation>
     </message>
     <message>
         <source>Limit tab width:</source>
