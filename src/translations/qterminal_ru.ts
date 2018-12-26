247bd2406a85bcd53aa2672c8e71a189b3022d3a
Golubev Alexander <fatzer2@gmail.com>
2018-12-26 18:16:01 +0000

    Translated using Weblate (Russian)
    Currently translated at 98.7% (154 of 156 strings)
    
    Translation: LXQt/QTerminal
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qterminal/ru/

--- src/translations/qterminal_ru.ts
+++ src/translations/qterminal_ru.ts
@@ -475,7 +475,7 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="415"/>
         <source>Terminal margin</source>
-        <translation type="unfinished"></translation>
+        <translation>Поля терминала</translation>
     </message>
     <message>
         <location filename="../forms/propertiesdialog.ui" line="458"/>
@@ -581,7 +581,7 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="385"/>
         <source>Fixed tab width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Постоянная ширина вкладки:</translation>
     </message>
     <message>
         <location filename="../forms/propertiesdialog.ui" line="655"/>
