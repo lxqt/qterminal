78e324327bd02363cbe42cb6faea3ffc9431dc5c
scootergrisen <scootergrisen@gmail.com>
2019-01-24 05:19:16 +0000

    Translated using Weblate (Danish)
    Currently translated at 100,0% (156 of 156 strings)
    
    Translation: LXQt/QTerminal
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qterminal/da/

--- src/translations/qterminal_da.ts
+++ src/translations/qterminal_da.ts
@@ -440,7 +440,7 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="326"/>
         <source>Cursor shape</source>
-        <translation type="unfinished"></translation>
+        <translation>Markørens form</translation>
     </message>
     <message>
         <location filename="../forms/propertiesdialog.ui" line="336"/>
