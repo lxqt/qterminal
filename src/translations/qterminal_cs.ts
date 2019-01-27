1a18efb77adb108035d977f8e4bc25650521172d
p-bo <pavel.borecki@gmail.com>
2019-01-27 17:01:50 +0000

    Translated using Weblate (Czech)
    Currently translated at 100,0% (156 of 156 strings)
    
    Translation: LXQt/QTerminal
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qterminal/cs/

--- src/translations/qterminal_cs.ts
+++ src/translations/qterminal_cs.ts
@@ -576,7 +576,7 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="326"/>
         <source>Cursor shape</source>
-        <translation type="unfinished"></translation>
+        <translation>Tvar kurzoru</translation>
     </message>
     <message>
         <location filename="../forms/propertiesdialog.ui" line="385"/>
