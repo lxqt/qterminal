36d953469342f3491e1e89a7f0a8d82f4e96a56f
Standreas <standreas@riseup.net>
2022-04-06 22:12:48 +0200

    Remove duplicate de_ch (#940)

--- src/translations/qterminal_de_CH.ts
+++ src/translations/qterminal_de_CH.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de">
+<TS version="2.1" language="de_CH">
 <context>
     <name>BookmarksWidget</name>
     <message>
