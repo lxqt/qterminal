fe149f36efe8f6e7ef79865e4296743c4181c737
Chih-Hsuan Yen <yan12125@gmail.com>
2022-06-18 20:34:42 +0800

    Add CI using GitHub Actions
    Ref: https://github.com/lxqt/lxqt/issues/1644

---
+++ .ci/build.sh
@@ -0,0 +1,9 @@
+set -ex
+
+source shared-ci/prepare-archlinux.sh
+
+# See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qterminal-git/PKGBUILD
+pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools qtermwidget-git qt5-x11extras qt5-translations
+
+cmake -B build -S .
+make -C build
