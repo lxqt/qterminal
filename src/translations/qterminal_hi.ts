a7cd863e868b8abca29e34a8dfeb050ee8f1cb46
Weblate <noreply@weblate.org>
2022-10-24 18:10:30 +0200

    Weblate commits
    Co-authored-by: Bashishtha Narayan Singh <bashishtha.singh@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/hi/
    Translation: LXQt Desktop/QTerminal

--- src/translations/qterminal_hi.ts
+++ src/translations/qterminal_hi.ts
@@ -228,7 +228,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="378"/>
         <source>&amp;New Window</source>
-        <translation type="unfinished"></translation>
+        <translation>नई विंडो (&amp;N)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="383"/>
