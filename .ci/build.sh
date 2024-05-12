32d2ef4ff15b8574c3ed928929c10959a632a9ef
Standreas <standreas@riseup.net>
2024-05-12 18:51:56 +0200

    Update build.sh

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,7 +3,7 @@ set -ex
 source shared-ci/prepare-archlinux.sh
 
 # See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/qterminal-git/PKGBUILD
-pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt5-tools qtermwidget-git qt5-x11extras qt5-translations libcanberra
+pacman -S --noconfirm --needed git cmake lxqt-build-tools-git qt6-tools qtermwidget-git qt6-translations libcanberra
 
 cmake -B build -S .
 make -C build
