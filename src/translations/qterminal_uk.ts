ac29d340c54d09f5e03a49d4af236bd79055ad7f
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-06-18 21:22:40 +0200

    Weblate commits (#1132)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal-menu-entry/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/hr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/uk/
    Translation: LXQt Desktop/QTerminal
    Translation: LXQt Desktop/QTerminal Menu Entry
    
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Peter  Jespersen <flywheel@illogical.dk>
    Co-authored-by: Pjetur G. Hjaltason <pjetur@pjetur.net>
    Co-authored-by: milotype <mail@milotype.de>

--- src/translations/qterminal_uk.ts
+++ src/translations/qterminal_uk.ts
@@ -712,17 +712,17 @@
     <message>
         <location filename="../forms/propertiesdialog.ui" line="627"/>
         <source>Word selection characters</source>
-        <translation type="unfinished"></translation>
+        <translation>Символи виділення слів</translation>
     </message>
     <message>
         <location filename="../forms/propertiesdialog.ui" line="634"/>
         <source>When selecting text by word, consider these characters as part of words in addition to alphanumeric characters</source>
-        <translation type="unfinished"></translation>
+        <translation>Виділяючи текст за словами, вважати ці символи частиною слів на додаток до буквенно-цифрових символів</translation>
     </message>
     <message>
         <location filename="../forms/propertiesdialog.ui" line="637"/>
         <source>:@-./_~</source>
-        <translation type="unfinished"></translation>
+        <translation>:@-./_~</translation>
     </message>
     <message>
         <location filename="../forms/propertiesdialog.ui" line="644"/>
