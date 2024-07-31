9fe0830a3cbf41403256c1b2eba9c0e7138b7025
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-07-31 20:35:39 +0200

    Weblate commits (#1160)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/pl/
    Translation: LXQt Desktop/QTerminal
    
    Co-authored-by: Ali Alhaidary علي الحيدري <ali.alhaidary@the5stars.org>
    Co-authored-by: Giannis Antypas <gianni.antypas@gmail.com>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: ovl-1 <ovl-002@mailinator.com>

--- src/translations/qterminal_nl.ts
+++ src/translations/qterminal_nl.ts
@@ -98,12 +98,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="294"/>
         <source>Split &amp;View Top-Bottom</source>
-        <translation type="unfinished"></translation>
+        <translation>Weerga&amp;ve boven en onder splitsen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="297"/>
         <source>Split Vie&amp;w Left-Right</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Weergave links en rechts splitsen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="302"/>
