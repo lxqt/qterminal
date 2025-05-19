f4b3f111f9086e565a1b5ed3a7f4732502c98b47
stefonarch <standreas@riseup.net>
2025-05-19 20:55:31 +0200

    Luganda...

--- src/translations/qterminal_lg.ts
+++ src/translations/qterminal_lg.ts
@@ -318,53 +318,48 @@
         <translation>En&amp;kula y&apos;akasimbannukuta</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="634"/>
+        <location filename="../mainwindow.cpp" line="633"/>
         <source>A process is running.</source>
         <translation>Waliwo omulimu ogukyabumbujja.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="683"/>
+        <location filename="../mainwindow.cpp" line="652"/>
         <source>Exit QTerminal</source>
         <translation>Komya QTerminal</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="625"/>
-        <source>Do not ask again</source>
-        <translation>Olulala kikolebwebukozi</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="683"/>
+        <location filename="../mainwindow.cpp" line="652"/>
         <source>Are you sure you want to exit?</source>
         <translation>Okakasa oyagala okumala?</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="706"/>
+        <location filename="../mainwindow.cpp" line="689"/>
         <source>About</source>
         <translation>Ebifa ku ScreenGrab</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="708"/>
+        <location filename="../mainwindow.cpp" line="691"/>
         <source>A lightweight and powerful multiplatform terminal emulator</source>
         <translation>Puloguramu ey&apos;ekiwandikiro ekitanyunyunta kompyuta era
 ekikolera ku sisitemu ez&apos;ebika ebisukka mu kimu</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="710"/>
+        <location filename="../mainwindow.cpp" line="693"/>
         <source>Copyright (C) </source>
         <translation>Lukusa lwa kukoppolola (C) </translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="710"/>
+        <location filename="../mainwindow.cpp" line="693"/>
         <source>2013-2025</source>
         <translation>2013-2025</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="712"/>
+        <location filename="../mainwindow.cpp" line="695"/>
         <source>LXQt Project</source>
         <translation>Kawefube wa LXQt</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="714"/>
+        <location filename="../mainwindow.cpp" line="697"/>
         <source>Development: </source>
         <translation>Abaawandika: </translation>
     </message>
