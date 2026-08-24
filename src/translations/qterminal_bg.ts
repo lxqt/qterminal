553f6cc122d4c21ef7bc3184d3176aa8aada72da
Weblate <noreply@weblate.org>
2026-08-24 08:29:33 +0200

    Weblate commits
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>
    Co-authored-by: BigELK176 ≡ <BigELK176@gmail.com>
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: bittin <bittin@reimu.nl>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qterminal/zh_TW/
    Translation: LXQt Desktop/QTerminal

--- src/translations/qterminal_bg.ts
+++ src/translations/qterminal_bg.ts
@@ -300,17 +300,17 @@
     <message>
         <location filename="../mainwindow.cpp" line="535"/>
         <source>&amp;Block</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Блок</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="536"/>
         <source>&amp;Underline</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Подчертаване</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="537"/>
         <source>&amp;I-Beam</source>
-        <translation type="unfinished"></translation>
+        <translation>I-образен</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="554"/>
@@ -978,17 +978,17 @@ Are you sure that you want to overwrite it?</source>
     <message>
         <location filename="../propertiesdialog.cpp" line="186"/>
         <source>Block</source>
-        <translation type="unfinished"></translation>
+        <translation>Блок</translation>
     </message>
     <message>
         <location filename="../propertiesdialog.cpp" line="186"/>
         <source>Underline</source>
-        <translation type="unfinished"></translation>
+        <translation>Подчертаване</translation>
     </message>
     <message>
         <location filename="../propertiesdialog.cpp" line="186"/>
         <source>I-Beam</source>
-        <translation type="unfinished"></translation>
+        <translation>I-образен</translation>
     </message>
     <message>
         <location filename="../propertiesdialog.cpp" line="461"/>
